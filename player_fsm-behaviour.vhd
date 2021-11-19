4a04b739ec9816f33afb6dd20ccead0e82aa79c4
VVerkoren <vinceverkoren@gmail.com>
2021-11-19 10:15:37 +0100

    Update player_fsm-behaviour.vhd

--- player_fsm-behaviour.vhd
+++ player_fsm-behaviour.vhd
@@ -6,9 +6,10 @@ architecture behaviour of player_fsm is
 
 type player_fsm_state is (	mine_state,right_state,left_state,down_state,
 				up_state, reset_state, central_state,
-				mine_up_state, mine_down_state, mine_right_state					, mine_left_state);
-	signal x_pos, y_pos, x_pos_next, y_pos_next: std_logic_vector(2 downto 0);
+				mine_up_state, mine_down_state, mine_right_state					, mine_left_state, lvl_up_state);
+	signal x_pos, y_pos, x_pos_next, y_pos_next: std_logic_vector(3 downto 0);
 	signal energy, energy_next: std_logic_vector(7 downto 0);
+	signal score, score_next: std_logic_vector(9 downto 0);
 
 	signal state, new_state:	player_fsm_state;
 
@@ -22,7 +23,13 @@ begin
 			else
 				state <= new_state;
 			end if;
+			
+			energy <= energy_next;
+			score <= score_next;
+			x_pos <= x_pos_next;
+			y_pos <= y_pos_next;
 		end if;
+		
 	end process;
 
 	process (state, reset, button_x_left, button_x_right, button_y_up,
@@ -31,13 +38,19 @@ begin
     	begin
         	case state is 
             		when reset_state =>
-			if (reset='0') then
-                    	new_state <= central_state;
-			else
-				new_state <= reset_state;
-			end if;
+			dir_mined <= "000";
+			x_pos_next <= "0100";
+			y_pos_next <= "0011";
+			new_state <= central_state;
+			score_next <= "0000000000";
+			energy_next <= "11001000";
 		
-		when central_state =>
+		when central_state => --central state where we read the inputs of the player
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			dir_mined <= "000";
+			score_next <= score;
+			energy_next <= energy;
 			if(button_mining = '1') then
 				new_state <= mine_state;
 			elsif(button_x_left = '1') then
@@ -50,7 +63,12 @@ begin
 				new_state <= down_state;
 			end if;
 		
-		when mine_state =>
+		when mine_state => --the mine button is pressed, now we read the input direction
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			dir_mined <= "000";
+			score_next <= score;
+			energy_next <= energy;
 			if(button_mining = '1' and button_x_left = '1') then
 				new_state <= mine_left_state;
 			elsif(button_mining = '1' and button_x_right = '1') then
@@ -61,104 +79,220 @@ begin
 				new_state <= mine_down_state;
 			end if;
 		
-		when mine_left_state =>
-			if(map_data_l = "001" or map_data_l = "010" or 
-			map_data_l = "100" or map_data_l = "111") then
+		when mine_left_state => --player mining to the left
+			new_state <= central_state;
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			if(map_data_l = "001") then
 				dir_mined <= "100"; --left mined (00)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
+			elsif(map_data_l = "010") then
+				dir_mined <= "100"; --left mined (00)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+'1');
+			elsif(map_data_l = "100") then
+				dir_mined <= "100"; --left mined (00)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+"010");
+			elsif(map_data_l = "111") then
+				dir_mined <= "100"; --left mined (00)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
 			else 
-				dir_mined <= "000";				
+				dir_mined <= "000";
+				energy_next <= energy;
+				score_next <= score;	
 				
 			end if;
-			new_state <= central_state;
+			
 		
-		when mine_right_state =>
-			if(map_data_r = "001" or map_data_r = "010" or 
-			map_data_r = "100" or map_data_r = "111") then
+		when mine_right_state => --player mining to the right
+
+			new_state <= central_state;
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			if(map_data_r = "001") then
 				dir_mined <= "101"; --right mined (01)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
+			elsif(map_data_r = "010") then
+				dir_mined <= "101"; --right mined (01)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+'1');
+			elsif(map_data_r = "100") then
+				dir_mined <= "101"; --right mined (01)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+"010");
+			elsif(map_data_r = "111") then
+				dir_mined <= "101"; --right mined (01)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
 			else 
-				dir_mined <= "000";				
+				dir_mined <= "000";
+				energy_next <= energy;
+				score_next <= score;	
 				
 			end if;
-			new_state <= central_state;
+			
 		
-		when mine_up_state =>
-			if(map_data_u = "001" or map_data_u = "010" or 
-			map_data_u = "100" or map_data_u = "111") then
+		when mine_up_state => --player mining up 
+			new_state <= central_state;
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			if(map_data_u = "001") then
 				dir_mined <= "110"; --up mined (10)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
+			elsif(map_data_u = "010") then
+				dir_mined <= "110"; --up mined (10)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+'1');
+			elsif(map_data_u = "100") then
+				dir_mined <= "110"; --up mined (10)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+"010");
+			elsif(map_data_u = "111") then
+				dir_mined <= "110"; --up mined (10)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
 			else 
-				dir_mined <= "000";				
+				dir_mined <= "000";
+				energy_next <= energy;
+				score_next <= score;	
 				
 			end if;
-			new_state <= central_state;
+			
 
-		when mine_down_state =>
-			if(map_data_d = "001" or map_data_d = "010" or 
-			map_data_d = "100" or map_data_d = "111") then
+		when mine_down_state => --player mining down
+			new_state <= central_state;
+			x_pos_next <= x_pos; 
+			y_pos_next <= y_pos;
+			if(map_data_d = "001") then
 				dir_mined <= "111"; --down mined (11)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
+			elsif(map_data_d = "010") then
+				dir_mined <= "111"; --down mined (11)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+'1');
+			elsif(map_data_d = "100") then
+				dir_mined <= "111"; --down mined (11)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= std_logic_vector(unsigned(score)+"010");
+			elsif(map_data_d = "111") then
+				dir_mined <= "111"; --down mined (11)
+				energy_next <= std_logic_vector(unsigned(energy)-"011");
+				score_next <= score;
 			else 
-				dir_mined <= "000";				
+				dir_mined <= "000";
+				energy_next <= energy;
+				score_next <= score;	
 				
 			end if;
-			new_state <= central_state;
+			
 
-		when left_state =>
+		when left_state => --move to the left
+			dir_mined <= "000";
+			y_pos_next <= y_pos;
+			score_next <= score;
+			
 			if(button_x_left = '1' and button_mining = '0') then
 				if(map_data_l = "000") then
 					x_pos_next <= std_logic_vector(unsigned(x_pos)-'1');
+					energy_next <= std_logic_vector(unsigned(energy)-'1');
 					new_state <= central_state;
+					
 				elsif(map_data_l = "101") then
-					--level_up <= '1';
-					new_state <= reset_state;
-
+					energy_next <= energy;
+					new_state <= lvl_up_state;
+					x_pos_next <= x_pos;
+					
 				else
 					new_state <= central_state;
+					energy_next <= energy;
+					x_pos_next <= x_pos;
 				end if;
 			end if;
 			
-		when right_state =>
+		when right_state => --move to the right
+			dir_mined <= "000";
+			y_pos_next <= y_pos;
+			score_next <= score;
 			if(button_x_right = '1' and button_mining = '0') then
 				if(map_data_r = "000") then
 					x_pos_next <= std_logic_vector(unsigned(x_pos)+'1');
+					energy_next <= std_logic_vector(unsigned(energy)-'1');
 					new_state <= central_state;
 				elsif(map_data_r = "101") then
-					--level_up <= '1';
-					new_state <= reset_state;
+					energy_next <= energy;
+					new_state <= lvl_up_state;
+					x_pos_next <= x_pos;
 
 				else
 					new_state <= central_state;
+					energy_next <= energy;
+					x_pos_next <= x_pos;
 				end if;
 			end if;
 
-		when up_state =>
+		when up_state => --move up
+			dir_mined <= "000";
+			x_pos_next <= x_pos;
+			score_next <= score;
 			if(button_y_up = '1' and button_mining = '0') then
 				if(map_data_u = "000") then
 					y_pos_next <= std_logic_vector(unsigned(y_pos)-'1');
+					energy_next <= std_logic_vector(unsigned(energy)-'1');
 					new_state <= central_state;
 				elsif(map_data_u = "101") then
-					--level_up <= '1';
-					new_state <= reset_state;
+					energy_next <= energy;
+					new_state <= lvl_up_state;
+					y_pos_next <= y_pos;
 
 				else
 					new_state <= central_state;
+					energy_next <= energy;
+					y_pos_next <= y_pos;
 				end if;
 			end if;		
 
-		when down_state =>
+		when down_state => --move down
+			dir_mined <= "000";
+			x_pos_next <= x_pos;
+			score_next <= score;
 			if(button_y_down = '1' and button_mining = '0') then
 				if(map_data_d = "000") then
 					y_pos_next <= std_logic_vector(unsigned(y_pos)+'1');
+					energy_next <= std_logic_vector(unsigned(energy)-'1');
 					new_state <= central_state;
 				elsif(map_data_d = "101") then
-					--level_up <= '1';
-					new_state <= reset_state;
+					energy_next <= energy
+					new_state <= lvl_up_state;
+					y_pos_next <= y_pos;
 
 				else
 					new_state <= central_state;
+					energy_next <= energy;
+					y_pos_next <= y_pos;
 				end if;
 			end if;	
 
+		when lvl_up_state => --level up by ladder, energy restored, score stays the same
+			dir_mined <= "000";
+			x_pos_next <= "0100";
+			y_pos_next <= "0011";
+			new_state <= central_state;
+			energy_next <= "11001000"
+			score_next <= score;
+
 		when others =>
 			new_state <= central_state;
+			dir_mined <= "000";
+			y_pos_next <= y_pos;
+			x_pos_next <= x_pos;
+			score_next <= score;
+			energy_next <= energy;
 
 
 
