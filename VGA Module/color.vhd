e4de6cd8040ceb8b3f9726e21b1f15a5a3a69db7
Ldamoiseaux <l.l.e.damoiseaux@student.tudelft.nl>
2021-11-25 12:10:23 +0100

    Colors Added

--- VGA Module/color.vhd
+++ VGA Module/color.vhd
@@ -32,51 +32,51 @@ else
 			red <= "000";
 			green <= "000";
 			blue <= "000";
-		-- Red --
+		-- White --
 		when "0001" =>
 			red <= "111";
-			green <= "000";
-			blue <= "000";
-		-- Green --
-		when "0011" =>
-			red <= "000";
 			green <= "111";
-			blue <= "000";
-		-- Blue --
-		when "0100" =>
-			red <= "000";
-			green <= "000";
 			blue <= "111";
-		-- Gray --
+		-- Background Red --
+		when "0011" =>
+			red <= "110";
+			green <= "010";
+			blue <= "000";
+		-- Dark Grey --
+		when "0100" =>
+			red <= "010";
+			green <= "010";
+			blue <= "010";
+		-- Light Grey --
 		when "0101" =>
-			red <= "100";
-			green <= "100";
-			blue <= "100";
-		-- Pink -- 
+			red <= "011";
+			green <= "011";
+			blue <= "011";
+		-- Orange -- 
 		when "0110" =>
 			red <= "111";
-			green <= "000";
-			blue <= "111";
+			green <= "100";
+			blue <= "000";
 		-- Yellow --
 		when "0111" =>
 			red <= "111";
-			green <= "111";
+			green <= "101";
 			blue <= "000";
-		-- Light Blue --
+		-- Denim Blue --
 		when "1000" =>
 			red <= "000";
+			green <= "010";
+			blue <= "110";
+		-- Mole Brown --
+		when "1001" =>
+			red <= "011";
+			green <= "010";
+			blue <= "010";
+		-- Diamond --
+		when "1010" =>
+			red <= "001";
 			green <= "111";
 			blue <= "111";
-		-- Dark Blue --
-		when "1001" =>
-			red <= "000";
-			green <= "000";
-			blue <= "100";
-		-- Dark Green --
-		when "1010" =>
-			red <= "000";
-			green <= "100";
-			blue <= "000";
 		-- **Placeholder** --
 		when "1011" =>
 			red <= "000";
@@ -97,11 +97,11 @@ else
 			red <= "000";
 			green <= "000";
 			blue <= "000";
-		-- White --
+		-- **Placeholder** --
 		when "1111" =>
-			red <= "111";
-			green <= "111";
-			blue <= "111";
+			red <= "000";
+			green <= "000";
+			blue <= "000";
 		-- Exception: use black
 		when others =>
 			red <= "000";
