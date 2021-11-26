636be0a188d07dfa3a4e30edefc093aeb49ff6ed
JRPost11 <jasper.r.post@gmail.com>
2021-11-26 09:06:39 +0100

    fixed spelling and spacing

--- VGA Module/texture_controller.vhd
+++ VGA Module/texture_controller.vhd
@@ -24,36 +24,36 @@ port (
 );
 end texture_controller;
 
--- Architecture of Controlle
+-- Architecture of Controller
 architecture behaviour of texture_controller is 
 	-- Constants: Timings.
-	constant H_DISPLAY : integer := 640;
-	constant H_FP : integer := 16;
-	constant H_SP : integer := 96;
-	constant H_BP : integer := 48;
+	constant H_DISPLAY 	: integer := 640;
+	constant H_FP 		: integer := 16;
+	constant H_SP 		: integer := 96;
+	constant H_BP 		: integer := 48;
 
-	constant V_DISPLAY : integer := 480;
-	constant V_FP : integer := 10;
-	constant V_SP : integer := 2;
-	constant V_BP : integer := 33;
+	constant V_DISPLAY 	: integer := 480;
+	constant V_FP 		: integer := 10;
+	constant V_SP 		: integer := 2;
+	constant V_BP 		: integer := 33;
 
 	-- Constants: For HS and VS intersection
-	constant H_MARGIN : integer := 40;
+	constant H_MARGIN 	: integer := 40;
 
-	constant pixel_num : integer := 16;
+	constant pixel_num 	: integer := 16;
 	
 	-- Signals
-	signal new_Hcount : unsigned(9 downto 0) := (others => '0');
-	signal new_Vcount : unsigned(9 downto 0) := (others => '0');
+	signal new_Hcount 	: unsigned(9 downto 0) := (others => '0');
+	signal new_Vcount 	: unsigned(9 downto 0) := (others => '0');
 	
-	signal Hcount : unsigned(9 downto 0) := (others => '0');
-	signal Vcount : unsigned(9 downto 0) := (others => '0');
+	signal Hcount 		: unsigned(9 downto 0) := (others => '0');
+	signal Vcount 		: unsigned(9 downto 0) := (others => '0');
 
-	signal Xposition : unsigned(3 downto 0) := (others => '0');
-	signal Yposition : unsigned(3 downto 0) := (others => '0');
+	signal Xposition 	: unsigned(3 downto 0) := (others => '0');
+	signal Yposition 	: unsigned(3 downto 0) := (others => '0');
 	
-	signal new_Xposition : unsigned(3 downto 0) := (others => '0');
-	signal new_Yposition : unsigned(3 downto 0) := (others => '0');
+	signal new_Xposition 	: unsigned(3 downto 0) := (others => '0');
+	signal new_Yposition 	: unsigned(3 downto 0) := (others => '0');
 	
 	begin
 	-- Process: Combinatorial
@@ -63,7 +63,7 @@ architecture behaviour of texture_controller is
 	
 
 	--Tile Type selector
-	if    (Xposition = unsigned(Xplayer) - 3 	and Yposition = unsigned(Yplayer)) 	then tile_type <= map_data();
+	if    (Xposition = unsigned(Xplayer) - 3 	and Yposition = unsigned(Yplayer)) 	then tile_type <= map_data();	-- **Mapdata is a placeholder** --
 	elsif (Xposition = unsigned(Xplayer) - 2 	and Yposition = unsigned(Yplayer) + 1) 	then tile_type <= map_data();
 	elsif (Xposition = unsigned(Xplayer) - 2 	and Yposition = unsigned(Yplayer)) 	then tile_type <= map_data();
 	elsif (Xposition = unsigned(Xplayer) - 2 	and Yposition = unsigned(Yplayer) - 1) 	then tile_type <= map_data();
@@ -91,51 +91,51 @@ architecture behaviour of texture_controller is
 	else tile_type <= map_data();
 	end if;
 
-	--Colum selector
+	--Column selector
 	case to_integer(Hcount) mod pixel_num is
-		when 0 => 	colum <= "00000";
-			  	new_Xposition <= Xposition + 1;
-		when 1 	=> colum <= "00001";
-		when 2 	=> colum <= "00010";
-		when 3 	=> colum <= "00011";
-		when 4 	=> colum <= "00100";
-		when 5 	=> colum <= "00101";
-		when 6 	=> colum <= "00110";
-		when 7 	=> colum <= "00111";
-		when 8 	=> colum <= "01000";
-		when 9 	=> colum <= "01001";
-		when 10 => colum <= "01011";
-		when 11 => colum <= "01111";
-		when 12 => colum <= "10000";
-		when 13 => colum <= "10001";
-		when 14 => colum <= "10011";
-		when 15 => colum <= "10111";
-		when others => 	colum 	<= (others => '0');
-			       	new_Xposition <= Xposition;
+		when 0 		=> colum <= "00000";
+			  	   new_Xposition <= Xposition + 1;
+		when 1 		=> colum <= "00001";
+		when 2 		=> colum <= "00010";
+		when 3 		=> colum <= "00011";
+		when 4 		=> colum <= "00100";
+		when 5 		=> colum <= "00101";
+		when 6 		=> colum <= "00110";
+		when 7 		=> colum <= "00111";
+		when 8 		=> colum <= "01000";
+		when 9 		=> colum <= "01001";
+		when 10 	=> colum <= "01011";
+		when 11 	=> colum <= "01111";
+		when 12 	=> colum <= "10000";
+		when 13 	=> colum <= "10001";
+		when 14 	=> colum <= "10011";
+		when 15 	=> colum <= "10111";
+		when others 	=> colum <= (others => '0');
+			       	   new_Xposition <= Xposition;
 	end case;
 
 
 	--Row selector
 	case to_integer(Vcount) mod pixel_num is
-		when 0 	=> 	row <= "00000";
-			 	new_Yposition <= Yposition + 1;
-		when 1 	=> row <= "00001";
-		when 2 	=> row <= "00010";
-		when 3 	=> row <= "00011";
-		when 4 	=> row <= "00100";
-		when 5 	=> row <= "00101";
-		when 6 	=> row <= "00110";
-		when 7 	=> row <= "00111";
-		when 8 	=> row <= "01000";
-		when 9 	=> row <= "01001";
-		when 10 => row <= "01011";
-		when 11 => row <= "01111";
-		when 12 => row <= "10000";
-		when 13 => row <= "10001";
-		when 14 => row <= "10011";
-		when 15 => row <= "10111";
-		when others => 	row <= (others => '0');
-				new_Yposition <= Yposition;
+		when 0 		=> row <= "00000";
+			 	   new_Yposition <= Yposition + 1;
+		when 1 		=> row <= "00001";
+		when 2 		=> row <= "00010";
+		when 3 		=> row <= "00011";
+		when 4 		=> row <= "00100";
+		when 5 		=> row <= "00101";
+		when 6 		=> row <= "00110";
+		when 7 		=> row <= "00111";
+		when 8 		=> row <= "01000";
+		when 9 		=> row <= "01001";
+		when 10 	=> row <= "01011";
+		when 11 	=> row <= "01111";
+		when 12 	=> row <= "10000";
+		when 13 	=> row <= "10001";
+		when 14 	=> row <= "10011";
+		when 15 	=> row <= "10111";
+		when others 	=> row <= (others => '0');
+				   new_Yposition <= Yposition;
 	end case;
 	
 	
