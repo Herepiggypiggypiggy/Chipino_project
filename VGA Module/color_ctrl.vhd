64fe7dd7ffccb0083c349c26a0e44dee063b3309
Ldamoiseaux <94607747+Ldamoiseaux@users.noreply.github.com>
2021-12-14 09:05:25 +0100

    changed to 5 bit addresses

--- VGA Module/color_ctrl.vhd
+++ VGA Module/color_ctrl.vhd
@@ -5,7 +5,7 @@ entity color_ctrl is
 port (
 	clk 		: in std_logic;
 	reset 		: in std_logic;
-	color_address 	: in std_logic_vector(3 downto 0);
+	color_address 	: in std_logic_vector(4 downto 0);
 
 	red 		: out std_logic_vector(3 downto 0);
 	green		: out std_logic_vector(3 downto 0);
