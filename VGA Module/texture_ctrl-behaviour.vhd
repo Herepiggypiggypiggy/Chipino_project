bf5ffc66411548c89d70209d1923e186c80cb99a
Ldamoiseaux <94607747+Ldamoiseaux@users.noreply.github.com>
2021-12-14 12:07:14 +0100

    added bg_select process

--- VGA Module/texture_ctrl-behaviour.vhd
+++ VGA Module/texture_ctrl-behaviour.vhd
@@ -310,7 +310,19 @@ architecture behaviour of texture_ctrl is
 
 	end process;
 
-
+	process(level)
+	begin
+		if (level = "00000000") then bg_select = "000";
+		elsif (level = "00000001" or level(7 downto 1) = "0000001") then bg_select = "001"; -- levels 1 to 3
+		elsif (level(7 downto 1) = "0000010" or level = "00000110") then bg_select = "010"; -- levels 4 to 6
+		elsif (level = "00000111" or level (7 downto 1) = "0000100") then bg_select = "011"; -- levels 7 to 9
+		elsif (level(7 downto 1)= "0001000" or level = "00010010") then bg_select = "100"; -- levels 10 to 12
+		elsif (level(7 downto 1)= "0001010" or level = "00010011") then bg_select = "101"; -- levels 13 to 15
+		elsif (level(7 downto 1)= "0001011" or level = "00011000") then bg_select = "110"; -- levels 16 to 18
+		elsif (level(7 downto 5)= "001" or level = "00011001") then bg_select = "111"; -- levels 19 to 22
+		else bg_select = "111";
+		end if;
+	end process;
 	
 	
 	-- Process: Sequential
