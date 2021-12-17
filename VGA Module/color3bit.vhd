4a18470cae94aa32ff132a55b022e2e1c464956a
Ldamoiseaux <94607747+Ldamoiseaux@users.noreply.github.com>
2021-12-17 09:59:40 +0100

    Update color3bit.vhd

--- VGA Module/color3bit.vhd
+++ VGA Module/color3bit.vhd
@@ -131,11 +131,11 @@ begin
 			red <= "111";
 			green <= "010";
 			blue <= "000";
-        -- Lightest grey --
-        when "10110" =>
-            red <= "110";
-            green <= "110";
-            blue <= "110";
+		-- Lightest grey --
+   		when "10110" =>
+   		       red <= "110";
+     		       green <= "110";
+     		       blue <= "110";
 
         -- *Placeholder* --
         when "10111" =>
