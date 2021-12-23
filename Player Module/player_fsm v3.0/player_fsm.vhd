c26d765624d266a74328befc05237c6421140a29
C0baltius <94438381+C0baltius@users.noreply.github.com>
2021-12-23 12:58:13 +0100

    updated entity player FSM
    added vga_done as an input

--- Player Module/player_fsm v3.0/player_fsm.vhd
+++ Player Module/player_fsm v3.0/player_fsm.vhd
@@ -15,15 +15,15 @@ entity player_fsm is
         CLK            : IN  std_logic;
         reset          : IN  std_logic;
         dir_mined      : OUT std_logic_vector(2 downto 0);
-        energy_lvl_out   : OUT std_logic_vector(8 downto 0);
-	score_out    : OUT std_logic_vector(9 downto 0);
-	level_out    : OUT std_logic_vector(4 downto 0);
+        energy_lvl_out : OUT std_logic_vector(8 downto 0);
+	score_out      : OUT std_logic_vector(9 downto 0);
+	level_out      : OUT std_logic_vector(4 downto 0);
 	level_d_out    : OUT std_logic_vector(7 downto 0);
 	score_d_out    : OUT std_logic_vector(15 downto 0);
 	energy_d_out   : OUT std_logic_vector(11 downto 0);
-        y_pos_out    : OUT std_logic_vector(3 downto 0);
-        x_pos_out    : OUT std_logic_vector(3 downto 0);
-	moved	: OUT std_logic;
-	game_state : OUT std_logic_vector(1 downto 0));
+        y_pos_out      : OUT std_logic_vector(3 downto 0);
+        x_pos_out      : OUT std_logic_vector(3 downto 0);
+	moved	       : OUT std_logic;
+	game_state     : OUT std_logic_vector(1 downto 0);
+	vga_done_out   : IN  std_logic);
 end player_fsm;
-
