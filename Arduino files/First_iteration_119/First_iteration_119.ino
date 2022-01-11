5a6c229d4fa3523d6823bf84497455258568114b
Herepiggypiggypiggy <60359643+Herepiggypiggypiggy@users.noreply.github.com>
2022-01-11 11:38:19 +0100

    Now includes SPI

--- Arduino files/First_iteration_119/First_iteration_119.ino
+++ Arduino files/First_iteration_119/First_iteration_119.ino
@@ -1,3 +1,12 @@
+#define SCK_PIN   13  // D13 = pin19 = PortB.5
+#define MISO_PIN  12  // D12 = pin18 = PortB.4
+#define MOSI_PIN  11  // D11 = pin17 = PortB.3
+#define SS_PIN    10  // D10 = pin16 = PortB.2
+
+#define UL unsigned long
+#define US unsigned short
+
+
 uint8_t currentX, currentY;
 uint8_t levelMap[5][16][16] = {0};
 uint8_t rockCount;
@@ -23,22 +32,68 @@ float diamondChance;
 //111  Rock with ladder
 //110  Empty
 
+void SlaveInit(void) {
+  pinMode(SCK_PIN, INPUT);
+  pinMode(MOSI_PIN, INPUT);
+  pinMode(MISO_PIN, OUTPUT);
+  pinMode(SS_PIN, INPUT);
+}
+
+unsigned short Read2Bytes(void) {
+    union {
+    unsigned short svar;
+    byte c[2];
+  } w;
+  
+  while(!(SPSR & (1<<SPIF))) ;
+  w.c[1] = SPDR;
+  while(!(SPSR & (1<<SPIF))) ;
+  w.c[0] = SPDR;             
+  return (w.svar);
+}
+unsigned short Read1Bytes(void) {
+    union {
+    unsigned short svar;
+    byte c[2];
+  } w;
+  
+  while(!(SPSR & (1<<SPIF))) ;
+  w.c[0] = SPDR;      
+  return (w.svar);
+}
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  SlaveInit();
+  delay(10);
+  Serial.println("SPI port reader v0.1"); 
   
 }
 
 void loop() 
 {
+  unsigned short word1;
+  byte flag1;
   for(l=0; l<31; l++)
   {
     createLevel();
     level++;
   }
   while(1){
-    delay(15000);
+    while (digitalRead(SS_PIN)==1) {}
+    
+    SPCR = (1<<SPE)|(0<<DORD)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA)|(0<<SPR1)|(1<<SPR0); // SPI on
+    word1 = Read2Bytes(); 
+    SPCR = (0<<SPE)|(0<<DORD)|(0<<MSTR)|(0<<CPOL)|(0<<CPHA)|(0<<SPR1)|(1<<SPR0);  // SPI off
+  
+    Serial.print(word1);
+    Serial.print('\n');
+    for(int k=0; k<24; k++)
+    {
+      SPDR= 011;
+      SPDR= 010;
+    }
   }
 }
 void createLevel()
