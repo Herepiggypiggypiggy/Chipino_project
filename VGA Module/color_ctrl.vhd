212814761e70462d81571c9c19a20bef48d1f0de
JRPost11 <jasper.r.post@gmail.com>
2022-01-13 20:44:35 +0100

    fixed layout

--- VGA Module/color_ctrl.vhd
+++ VGA Module/color_ctrl.vhd
@@ -3,12 +3,12 @@ use IEEE.std_logic_1164.all;
 
 entity color_ctrl is
 port (
-	clk 		: in std_logic;
-	reset 		: in std_logic;
+	clk 			: in std_logic;
+	reset 			: in std_logic;
 	color_address 	: in std_logic_vector(4 downto 0);
 
-	red 		: out std_logic_vector(3 downto 0);
-	green		: out std_logic_vector(3 downto 0);
-	blue 		: out std_logic_vector(3 downto 0)
+	red 			: out std_logic_vector(3 downto 0);
+	green			: out std_logic_vector(3 downto 0);
+	blue 			: out std_logic_vector(3 downto 0)
 );
 end color_ctrl;
